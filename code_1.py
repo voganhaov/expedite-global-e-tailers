be70f4a0685d805f1725b5b368494b1051095765
voganhaov <152450101+voganhaov@users.noreply.github.com>
2024-01-20 22:45:06 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,45 +1,28 @@
-main();
-    for (let i = 0; i < 10; i++) {
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    });
     const data = [];
-
+main();
+    const data = generateRandomData();
+    data.forEach(item => {
         console.log("Random Number: " + item);
-    });
-    }
 
-    return data;
-}
-    const data = generateRandomData();
 
-    data.forEach(item => {
-}
-function main() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-function generateRandomData() {        console.log("Random Number: " + item);
-    });
     for (let i = 0; i < 10; i++) {
-
-
-
-    data.forEach(item => {
-    return data;
-function main() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-main();
-    }
-function generateRandomData() {
-    const data = [];
 }
-}}
-            System.out.println("Random Number: " + item);
+    }
 
+}
+function generateRandomData() {        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+}
         for (int item : data) {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
         }
     }
-        for (int i = 0; i < 10; i++) {
-
-        int[] data = new int[10];
 public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
